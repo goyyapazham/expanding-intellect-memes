1cdeb39b303be33a7655c85445a124ca536ab4c8
Elias Milborn <emilborn@stuy.edu>
2017-05-25 21:36:39 -0400

    removed bug testing line

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def authenticate():
         auth_code = request.args.get('code')
         credentials = flow.step2_exchange(auth_code) # This is step two authentication to get the code and store the credentials in a credentials object
         session['credentials'] = credentials.to_json() # Converts the credentials to json and stores it in the session variable
-        login.getEmail(session)
         return redirect(url_for('about'))
 
 if __name__ == '__main__':
