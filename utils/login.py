b7f60f79be5f20b67928e7ba6f9bd3ccc3b2c573
goyyapazham <nalanda99@gmail.com>
2017-06-06 22:37:00 -0400

    b

--- utils/login.py
+++ utils/login.py
@@ -3,7 +3,7 @@ import json # Json library to handle replies
 from httplib2 import Http
 
 
-ADMINS = ["emilborn@stuy.edu", "dw@stuycs.org", "nalandasharadjaya@gmail.com", "eliasmilborn@gmail.com", "cathanitis@stuy.edu"]
+ADMINS = ["emilborn@stuy.edu", "dw@stuycs.org", "nsharadjaya@stuy.edu", "eliasmilborn@gmail.com", "cathanitis@stuy.edu"]
 
 #given a session, returns whethere the user is logged in
 def loggedIn(session):
